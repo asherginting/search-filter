01df9e54a17f0b042675eb58655247ef5bd0733a
Asher Azriel Ginting <asherginting3@gmail.com>
2023-05-13 14:17:08 +0700

    feat: add searchfilter simple

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,20 +1,91 @@
 <template>
-    <div class="hello">
-      <h1>{{ msg }}</h1>
+  <div class="search-filter">
+    <h2 class="search-filter-title">Search Your Name</h2>
+    <div class="search-filter-input">
+      <input type="text" v-model="searchQuery" placeholder="Search ...." class="search-filter__input">
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
+    <div class="search-filter-list">
+      <ul class="search-filter-name">
+        <li v-for="(name, index) in filteredNames" :key="index" class="search-filter-listname">{{ name }}</li>
+        <li v-if="filteredNames.length === 0" class="search-filter-notfound">No names match!</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchFilter',
+  props: {
+    names: Array 
+  },
+  data() {
+    return {
+      searchQuery: ''
+    }
+  },
+  computed: {
+    filteredNames() {
+      return this.names.filter(name =>
+        name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      )
     }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.search-filter {
+  max-width: 600px;
+  margin: 0 auto;
+  font-family: Arial, sans-serif;
+}
+
+.search-filter-title {
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+
+.search-filter-input {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  border-bottom: 2px solid #eee;
+}
+
+.search-filter__input {
+  flex: 1;
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  background-color: transparent;
+  outline: none;
+}
+
+.search-filter-list {
+  max-height: 600px; 
+  overflow-y: auto;
+}
+
+.search-filter-name {
+  list-style-type: none;
+  padding: 0;
+}
+
+.search-filter-listname {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+.search-filter-listname:hover {
+  background-color: #f5f5f5;
+}
+
+.search-filter-notfound {
+  color: #aaa;
+  font-size: 14px;
+  margin-top: 20px;
+}
+</style>
